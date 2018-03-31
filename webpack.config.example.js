f646f16871217e2bd62d756559844ea535f08f8a
Arnaud BOEGLIN <a.boeglin67@gmail.com>
2018-03-31 18:55:30 +0200

    Fix webpack-dev-server error at launch

--- webpack.config.example.js
+++ webpack.config.example.js
@@ -21,7 +21,8 @@ module.exports = {
     devServer: {
         contentBase: path.join(__dirname, "/example"),
         compress: true,
-        port: 8080
+        port: 8080,
+        open: true
     },
     module: {
         rules: [
