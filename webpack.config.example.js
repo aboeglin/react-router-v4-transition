2da94c4ff87c2ae883a3fd328a6b06a137ef064d
Arnaud BOEGLIN <a.boeglin67@gmail.com>
2018-03-31 18:57:07 +0200

    Reformat webpack config file

--- webpack.config.example.js
+++ webpack.config.example.js
@@ -1,43 +1,43 @@
-var webpack = require('webpack');
-var path = require('path');
-var CopyWebpackPlugin = require('copy-webpack-plugin');
+const webpack = require('webpack');
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-    watch: true,
-    devtool: 'cheap-module-source-map',
-    entry: {
-        bundle: path.resolve(__dirname, 'src/example/index.js')
-    },
-    output: {
-        path: path.resolve(__dirname, 'example'),
-        filename: "bundle.js"
-    },
-    plugins: [
-        new CopyWebpackPlugin([
-            {from: 'src/example/index.html', to: 'index.html'},
-            {from: 'src/example/main.css', to: 'main.css'}
-        ])
-    ],
-    devServer: {
-        contentBase: path.join(__dirname, "/example"),
-        compress: true,
-        port: 8080,
-        open: true
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader'
-            }
-        ]
-    },
-    resolve: {
-        extensions: ['.js'],
-        modules: [
-            path.resolve(__dirname, 'src'),
-            path.resolve(__dirname, 'node_modules')
-        ]
-    }
+	watch: true,
+	devtool: 'cheap-module-source-map',
+	entry: {
+		bundle: path.resolve(__dirname, 'src/example/index.js')
+	},
+	output: {
+		path: path.resolve(__dirname, 'example'),
+		filename: "bundle.js"
+	},
+	plugins: [
+		new CopyWebpackPlugin([
+			{from: 'src/example/index.html', to: 'index.html'},
+			{from: 'src/example/main.css', to: 'main.css'}
+		])
+	],
+	devServer: {
+		contentBase: path.join(__dirname, "/example"),
+		compress: true,
+		port: 8080,
+		open: true
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: 'babel-loader'
+			}
+		]
+	},
+	resolve: {
+		extensions: ['.js'],
+		modules: [
+			path.resolve(__dirname, 'src'),
+			path.resolve(__dirname, 'node_modules')
+		]
+	}
 };
